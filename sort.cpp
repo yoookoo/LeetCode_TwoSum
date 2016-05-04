e367fe52e074c4feb601be4530bdcb4d8dc6e5ea
yoookoo <448064885@qq.com>
2016-05-04 19:29:59 +0800

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //建一个unordered_map的hash表
         unordered_map<int, int> hash;
         vector<int> result;
         for (int i = 0; i < nums.size(); i++) {
             int numberToFind = target - nums[i];
-            if (hash.find(numberToFind) != hash.end()) {
+            if (hash.find(numberToFind) != hash.end()) {  //在hash表中找到numberToFind则为true，否则为false
                 result.push_back(hash[numberToFind]);
-                result.push_back(i);
+                result.push_back(i);          //向result中加入numberToFind和i
                 return result;
             }
             hash[nums[i]] = i;
